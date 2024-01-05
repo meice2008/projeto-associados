d97f897e2f1e307337bca1c355553f31b6a37e51
Meice Silva <meice2008@gmail.com>
2024-01-04 22:23:24 -0300

    iniciando projeto com asp.net mvc

---
+++ ProjetoAssociados/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoAssociados.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
