d014b05a8eeffc4d1c459f759508d7c70386bce6
Meice Silva <meice2008@gmail.com>
2024-01-05 15:10:13 -0300

    cadastro de empresas e index

---
+++ ProjetoAssociados/Controllers/EmpresasController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAssociados.Data;
+using ProjetoAssociados.Models;
+
+namespace ProjetoAssociados.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        readonly private ApplicationDbContext _context;
+
+        public EmpresasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<EmpresaModel> empresas = _context.Empresas;
+            return View(empresas);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmpresaModel empresaModel)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Empresas.Add(empresaModel);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? Id)
+        {
+
+            if(Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            EmpresaModel empresaModel = _context.Empresas.FirstOrDefault(x => x.Id == Id);
+
+            if(empresaModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(empresaModel);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmpresaModel empresaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Empresas.Update(empresaModel);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(empresaModel);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if(id == null || id ==0)
+            {
+                return NotFound();
+            }
+
+            EmpresaModel empresaModel = _context.Empresas.FirstOrDefault(x=>x.Id == id);
+
+            if(empresaModel == null)
+            {
+                return NotFound(empresaModel);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Excluir (EmpresaModel empresaModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Empresas.Remove(empresaModel);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(empresaModel);
+        }
+
+    }
+}
