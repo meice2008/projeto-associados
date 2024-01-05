bc41be0a3f5d3bad9f7aa6336b5e77bcf64980aa
Meice Silva <meice2008@gmail.com>
2024-01-05 00:04:25 -0300

    [final-do-dia] app conectado ao banco + tela index

---
+++ ProjetoAssociados/Controllers/AssociadosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAssociados.Data;
+using ProjetoAssociados.Models;
+
+namespace ProjetoAssociados.Controllers
+{
+    public class AssociadosController : Controller
+    {
+        readonly private ApplicationDbContext _context;
+        public AssociadosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<AssociadoModel> associados = _context.Associados;
+            return View(associados);
+        }
+    }
+}
---
+++ ProjetoAssociados/Models/AssociadoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoAssociados.Models
+{
+    public class AssociadoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DtNascimento { get; set; }
+    }
+}
